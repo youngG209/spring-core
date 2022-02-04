c3bd788936999212763fcaaa3b97f93e97fe0f68
youngG209 <46043956+youngG209@users.noreply.github.com>
2022-02-04 15:16:01 +0900

    스프링 핵심 원리 이해1 - 예제 만들기

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,16 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+    private int dicountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return dicountFixAmount;
+        }
+        return 0;
+    }
+}
---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,24 @@
+package hello.core.member;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        //given
+        Member member = new Member(1L, "member1", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+}
