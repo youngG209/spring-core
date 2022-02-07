b91e8a57aa018fb5b048db71bcb7c4d44816771a
youngG209 <46043956+youngG209@users.noreply.github.com>
2022-02-07 16:30:00 +0900

    빈 스코프  - 웹 스코프  - request 스코프 예제 만들기   - 스코프와 Provider

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,12 +1,14 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -39,6 +41,34 @@ public class SingletonWithPrototypeTest1 {
         assertThat(count2).isEqualTo(2);
     }
 
+    @Test
+    void singletonClientUseObjectProvide() {
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(ClientBean2.class, PrototypeBean.class);
+
+        ClientBean2 clientBean1 = ac.getBean(ClientBean2.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean2 clientBean2 = ac.getBean(ClientBean2.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUseProvider() {
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(ClientBean3.class, PrototypeBean.class);
+
+        ClientBean3 clientBean1 = ac.getBean(ClientBean3.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean3 clientBean2 = ac.getBean(ClientBean3.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
+    }
+
     @Scope("singleton")
     static class ClientBean {
         private final PrototypeBean prototypeBean; // 생성시점에 주입
@@ -49,6 +79,35 @@ public class SingletonWithPrototypeTest1 {
         }
 
         public int logic() {
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+
+    @Scope("singleton")
+    static class ClientBean2 {
+
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+//        @Autowired
+//        private ObjectFactory<PrototypeBean> prototypeBeanFactory;
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+
+    @Scope("singleton")
+    static class ClientBean3 {
+
+        @Autowired
+        private Provider<PrototypeBean> provider;
+        public int logic() {
+            PrototypeBean prototypeBean = provider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
